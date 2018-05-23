631907c624ea1ce99447ce3a9f62236df78806c4
EmanMacario <emacario1997@hotmail.com>
2018-05-24 00:13:39 +1000

    More cleanup and comments

--- certcheck.c
+++ certcheck.c
@@ -1,11 +1,12 @@
-/* 
+/**
  * COMP30023 Computer Systems
  * Semester 1, 2018
- * Assignment 2 - Certificate Validation using OpenSSL
+ * Assignment 2 - TLS Certificate Validation Using OpenSSL
  *
  * Name: Emmanuel Macario <macarioe@student.unimelb.edu.au>
  * Student Number: 831659
- * Last Modified: 21/05/18
+ * Filename: certcheck.c
+ * Last Modified: 23/05/18
  *
  */
 #include <stdio.h>
@@ -22,7 +23,8 @@
 
 /** 
  * Gives the user information on how to use the program 
- * and the required input command line arguments.
+ * and the required input command line arguments. Then
+ * exits the program.
  */
 void usage_exit(char *prog_name) {
     printf("Usage: %s [path to csv file]\n", prog_name);
@@ -30,24 +32,35 @@ void usage_exit(char *prog_name) {
 }
 
 
+/**
+ * Takes as input an X.509 certificate, and returns
+ * true if the respective RSA key length is greater than 
+ * or equal to the default minimum length of 2048 bits.
+ */
 bool validate_rsa_key_size(X509 *cert) {
 
     // Get size of public key modulus in bits.
     EVP_PKEY *pkey = X509_get_pubkey(cert);
     int key_type = EVP_PKEY_type(pkey->type);
     assert(key_type == EVP_PKEY_RSA);
-
     int keysize = BN_num_bits(pkey->pkey.rsa->n);
     EVP_PKEY_free(pkey);
 
-    if (keysize == RSA_KEYSIZE_DEFAULT) {
+    if (keysize >= RSA_MINIMUM_KEYSIZE) {
         return true;
     }
     return false;
 }
 
 
+/**
+ * Takes as input an X.509 certificate and
+ * returns if the 'Not Before' date is valid,
+ * otherwise returns false.
+ */
 bool validate_not_before(X509 *cert) {
+    assert(cert != NULL);
+
     int day, sec;
 
     ASN1_TIME *not_before = X509_get_notBefore(cert);
@@ -60,7 +73,14 @@ bool validate_not_before(X509 *cert) {
 }
 
 
+/**
+ * Takes as input an X.509 certificate and
+ * returns if the 'Not After' date is valid,
+ * otherwise returns false.
+ */
 bool validate_not_after(X509 *cert) {
+    assert(cert != NULL);
+
     int day, sec;
 
     ASN1_TIME *not_after = X509_get_notAfter(cert);
@@ -73,24 +93,41 @@ bool validate_not_after(X509 *cert) {
 }
 
 
+/**
+ * Takes as input an X.509 certificate and returns true
+ * if both the 'Not Before' and 'Not After' dates are valid,
+ * otherwise returns false.
+ */
 bool validate_dates(X509 *cert) {
+    assert(cert != NULL);
     return validate_not_before(cert) && validate_not_after(cert);
 }
 
 
-bool hostmatch(char *domain_name, char *pattern) {
+/**
+ * Takes as input a domain name as seen in the second column of the input
+ * csv file, and validates it against another DNS name 'pattern', which may 
+ * or may not include a wilcard. Returns true if there is a match, otherwise
+ * returns false.
+ */
+bool matches(char *domain_name, char *pattern) {
     assert(domain_name != NULL && pattern != NULL);
 
     char *pattern_wildcard, *pattern_label_end, *domain_name_label_end;
 
-    // If no wildcard, just if they are the same.
+    // Look for a wildcard in the pattern.
     pattern_wildcard = strchr(pattern, '*');
+
+    // If there is no wildcard, just perform a case insensitive
+    // comparison of the domain name and pattern, and return
+    // whether they match or not.
     if (pattern_wildcard == NULL) {
         return !strcasecmp(domain_name, pattern) ? true : false;
     }
 
-    // Ensure at minimum two periods are present in pattern to avoid 'too wide'
-    // wildcard domains.
+    // Now, we know there exists a wildcard in the pattern. Ensure at minimum 
+    // two periods are present in pattern to avoid wildcard domains that are
+    // 'too wide'.
     // i.e. A cert with '*' plus a TLD is not allowed (e.g. '*.com').
     //      A cert with just '*' is too general and is not allowed.
     pattern_label_end = strchr(pattern, '.');
@@ -105,9 +142,10 @@ bool hostmatch(char *domain_name, char *pattern) {
         return false;
     }
 
-    // Now, check if the domain name matches the pattern. Ensures that
-    // only a single level of subdomain matching is supported. This is because
-    // the wildcard only covers one level of subdomains.
+    // Now, check if the domain name is valid, with respect to the wildcard 
+    // domain (i.e. the 'pattern'). This check ensures that only a single 
+    // level of subdomain matching is supported, since a wildcard only covers 
+    // one level of subdomains.
     if (domain_name_label_end == NULL ||
         strcasecmp(domain_name_label_end, pattern_label_end)) {
         return false;
@@ -118,7 +156,11 @@ bool hostmatch(char *domain_name, char *pattern) {
 }
 
 
-
+/**
+ * Performs a validation check to see if a given domain name
+ * matches the certificate's 'Common Name' subject field entry. 
+ * Returns true if there is a match, otherwise returns false.
+ */
 bool matches_common_name(char *domain_name, X509 *cert) {
 
     X509_NAME *subject = X509_get_subject_name(cert);
@@ -155,16 +197,21 @@ bool matches_common_name(char *domain_name, X509 *cert) {
         exit(EXIT_FAILURE);
     }
 
-    return hostmatch(domain_name, common_name);
+    // Return if there was a match.
+    return matches(domain_name, common_name);
 }
 
 
+/**
+ * Performs a validation check to see if a given domain name
+ * matches any of the hostnames in the certificate's 'Subject
+ * Alternative Name' extension.
+ */
 bool matches_subject_alt_name(char *domain_name, X509 *cert) {
 
     // Get the Subject Alternative Names (if they exist).
     STACK_OF(GENERAL_NAME) *sans =
                  X509_get_ext_d2i(cert, NID_subject_alt_name, NULL, NULL);
-
 
     // If certificate does not contain SAN extension, just return false.
     if (sans == NULL) {
@@ -173,17 +220,17 @@ bool matches_subject_alt_name(char *domain_name, X509 *cert) {
 
     int n_sans = sk_GENERAL_NAME_num(sans);
 
-    // For each SAN, see if we get a match!
+    // For each SAN, see if the domain name matches!
     bool match = false;
     for (int i = 0; i < n_sans; i++) {
 
         GENERAL_NAME *san = sk_GENERAL_NAME_value(sans, i);
 
+        // Only need to check DNS names.
         if (san->type == GEN_DNS) {
-            // Current SAN is a DNS, need to check it.
             char *dns_name = (char *)ASN1_STRING_data(san->d.dNSName);
             
-            if (hostmatch(domain_name, dns_name)) {
+            if (matches(domain_name, dns_name)) {
                 match = true;
                 break;
             }
@@ -197,69 +244,38 @@ bool matches_subject_alt_name(char *domain_name, X509 *cert) {
 }
 
 
+/**
+ * Performs a validation check to see if the given domain name
+ * matches the certificate's 'Common Name' entry, or any of the hostnames 
+ * in the certificate's 'Subject Alternative Name' extension. Returns
+ * true if the certificate is valid for the given domain name, otherwise
+ * returns false.
+ */
 bool validate_domain_name(char *domain_name, X509 *cert) {
     assert(domain_name != NULL && cert != NULL);
 
-    // First try to see if domain names matches CN.
-    bool match = matches_common_name(domain_name, cert);
+    // First try to see if domain names matches CN. If domain name 
+    // doesn't match CN, check for a match  in SANs (only if the 
+    // certificate contains SAN extension).
+    bool match = matches_common_name(domain_name, cert) ||
+                 matches_subject_alt_name(domain_name, cert);
 
-    // If domain name doesn't match CN, check for a match 
-    // in SANs (only if certificate contains SAN extension).
-    if (!match) {
-        match = matches_subject_alt_name(domain_name, cert);
-    }
     return match;
 }
 
 
-
+/**
+ * Performs a validation check to see if the certificate's 'Basic Constraints'
+ * extension contains the value 'CA:FALSE'. Returns true if so, otherwise 
+ * returns false.
+ */
 bool validate_basic_constraints(X509 *cert) {
 
-    int index = X509_get_ext_by_NID(cert, NID_basic_constraints, -1);
-    X509_EXTENSION *ex = X509_get_ext(cert, index);
+    // Get the 'Basic Constrains' extension.
+    int basic_constraints_index = 
+                X509_get_ext_by_NID(cert, NID_basic_constraints, -1);
 
-    ASN1_OBJECT *obj = X509_EXTENSION_get_object(ex);
-    if (obj == NULL) {
-        fprintf(stderr, "Unable to extract ASN1 object from extension");
-    }
-
-    BIO *ext_bio = BIO_new(BIO_s_mem());
-    if (ext_bio == NULL) {
-        fprintf(stderr, "Unable to allocate memory for extension value BIO");
-    }
-
-    if (!X509V3_EXT_print(ext_bio, ex, 0, 0)) {
-        M_ASN1_OCTET_STRING_print(ext_bio, ex->value);
-    }
-
-
-    BUF_MEM *bptr = NULL;
-    BIO_flush(ext_bio);
-    BIO_get_mem_ptr(ext_bio, &bptr);
-
-    char *data = (char *)malloc((bptr->length + 1) * sizeof(char));
-    memcpy(data, bptr->data, bptr->length);
-    data[bptr->length] = '\0';
-
-    bool valid;
-    if (strstr(data, BC_DEFAULT) == NULL) {
-        valid = false;
-    } else {
-        valid = true;
-    }
-
-    BIO_free_all(ext_bio);
-    free(data);
-
-    return valid;
-}
-
-
-
-bool validate_ext_key_usage(X509 *cert) {
-
-    int index = X509_get_ext_by_NID(cert, NID_ext_key_usage, -1);
-    X509_EXTENSION *ex = X509_get_ext(cert, index);
+    X509_EXTENSION *ex = X509_get_ext(cert, basic_constraints_index);
 
     ASN1_OBJECT *obj = X509_EXTENSION_get_object(ex);
     if (obj == NULL) {
@@ -271,6 +287,7 @@ bool validate_ext_key_usage(X509 *cert) {
         fprintf(stderr, "Unable to allocate memory for extension value BIO\n");
     }
 
+    // Write the extension value to a buffer.
     if (!X509V3_EXT_print(ext_bio, ex, 0, 0)) {
         M_ASN1_OCTET_STRING_print(ext_bio, ex->value);
     }
@@ -279,12 +296,16 @@ bool validate_ext_key_usage(X509 *cert) {
     BIO_flush(ext_bio);
     BIO_get_mem_ptr(ext_bio, &bptr);
 
+    // Null terminate the value so that we can perform string comparison.
     char *data = (char *)malloc((bptr->length + 1) * sizeof(char));
+    assert(data != NULL);
     memcpy(data, bptr->data, bptr->length);
     data[bptr->length] = '\0';
 
+    // Simply check if the data entry contains the value 
+    // needed for the certificate to be 'valid'.
     bool valid;
-    if (strstr(data, EKU_DEFAULT) == NULL) {
+    if (strstr(data, BASIC_CONSTRAINTS_DEFAULT) == NULL) {
         valid = false;
     } else {
         valid = true;
@@ -297,6 +318,65 @@ bool validate_ext_key_usage(X509 *cert) {
 }
 
 
+/**
+ * Performs a validation check to see if the certificate's 'Extended Key Usage'
+ * extension contains the value 'TLS Web Server Authentication'. Returns true 
+ * if so, otherwise returns false.
+ */
+bool validate_ext_key_usage(X509 *cert) {
+
+    // Get the 'Extended Key Usage' extension.
+    int ext_key_usage_index = X509_get_ext_by_NID(cert, NID_ext_key_usage, -1);
+    X509_EXTENSION *ex = X509_get_ext(cert, ext_key_usage_index);
+
+    ASN1_OBJECT *obj = X509_EXTENSION_get_object(ex);
+    if (obj == NULL) {
+        fprintf(stderr, "Unable to extract ASN1 object from extension\n");
+    }
+
+    BIO *ext_bio = BIO_new(BIO_s_mem());
+    if (ext_bio == NULL) {
+        fprintf(stderr, "Unable to allocate memory for extension value BIO\n");
+    }
+
+    // Write the extension value to a buffer.
+    if (!X509V3_EXT_print(ext_bio, ex, 0, 0)) {
+        M_ASN1_OCTET_STRING_print(ext_bio, ex->value);
+    }
+
+    BUF_MEM *bptr = NULL;
+    BIO_flush(ext_bio);
+    BIO_get_mem_ptr(ext_bio, &bptr);
+
+    // Null terminate the value so that we can perform string comparison.
+    char *data = (char *)malloc((bptr->length + 1) * sizeof(char));
+    assert(data != NULL);
+    memcpy(data, bptr->data, bptr->length);
+    data[bptr->length] = '\0';
+
+    // Simply check if the data entry contains the value 
+    // needed for the certificate to be 'valid'.
+    bool valid;
+    if (strstr(data, EXTENDED_KEY_USAGE_DEFAULT) == NULL) {
+        valid = false;
+    } else {
+        valid = true;
+    }
+
+    BIO_free_all(ext_bio);
+    free(data);
+
+    return valid;
+}
+
+
+/**
+ * Takes as input the name of a file containing a Base64 encoded X.509
+ * certificate, and validates it with respect to associated domain name 
+ * given in the second column of the input csv file. Also validates the
+ * certificate's dates, RSA key size, basic constraints and extended key 
+ * usage values. Returns 1 if the certificate is valid, otherwise returns 0.
+ */
 int validate_certificate(char *filename, char *domain_name) {
 
     BIO *certificate_bio = NULL;
@@ -315,7 +395,9 @@ int validate_certificate(char *filename, char *domain_name) {
         exit(EXIT_FAILURE);
     }
 
-    // Validate or invalidate the certificate.
+    // Validate or invalidate the certificate, with respect to the domain 
+    // name. Check if the certificate satisfies the constraints of a 'valid' 
+    // certificate.
     bool valid = validate_dates(cert) &&
                  validate_domain_name(domain_name, cert) &&
                  validate_rsa_key_size(cert) &&
@@ -325,19 +407,28 @@ int validate_certificate(char *filename, char *domain_name) {
     X509_free(cert);
     BIO_free_all(certificate_bio);
 
+    // Return the corresponding integer value, denoting 
+    // whether the certificate is valid, in the output file.
     return valid ? 1 : 0;
 }
 
 
-/** MAIN PROGRAM **/
+/**
+ * The main program, responsible for opening and parsing the input 
+ * csv file line by line, validating certificates one by one while
+ * appending results to the output csv file. This program sequentially 
+ * processes certificates instead of caching the entire contents of the 
+ * csv file into a dynamic allocated structure, since there is no upper 
+ * bound on the total number of certificates to be validated.
+ */
 int main(int argc, char *argv[]) {
 
-    // Check command line arguments supplied.
+    // Check that command line arguments were supplied.
     if (argc != 2) {
         usage_exit(argv[0]);
     }
 
-    // Name of input csv file.
+    // Get the name of the path to the input csv file.
     char *input_csv = argv[1];
 
     // Try opening the input csv file.
@@ -352,7 +443,6 @@ int main(int argc, char *argv[]) {
     ERR_load_BIO_strings();
     ERR_load_crypto_strings();
 
-
     // Get output file ready for writing into.
     FILE *out = fopen("output.csv", "w");
     if (out == NULL) {
@@ -360,12 +450,12 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    char filename[MAX_LENGTH], domain[MAX_LENGTH], line[MAX_LENGTH];
+    char filename[MAX_LINE_LEN], domain[MAX_LINE_LEN], line[MAX_LINE_LEN];
 
-
-    // Parse and process each line of the input csv 
-    // file one at a time.
-    while (fgets(line, MAX_LENGTH, in) != NULL) {
+    // Parse and process each line of the input csv.
+    // Validate certificates one at a time, appending
+    // results to the output file.
+    while (fgets(line, MAX_LINE_LEN, in) != NULL) {
 
         // Get certificate filename and the domain name.
         if (sscanf(line, "%[^,],%s\n", filename, domain) != 2) {
@@ -373,7 +463,7 @@ int main(int argc, char *argv[]) {
             exit(EXIT_FAILURE);
         }
 
-        // Check if the certificate is valid with respect to the domain name.
+        // Validate the certificate with respect to the domain name.
         int valid = validate_certificate(filename, domain);
 
         // Print result to the output file.
--- certcheck.h
+++ certcheck.h
@@ -1,21 +1,96 @@
-/* 
+/**
  * COMP30023 Computer Systems
  * Semester 1, 2018
- * Assignment 2 - Certificate Validation using OpenSSL
+ * Assignment 2 - TLS Certificate Validation Using OpenSSL
  *
  * Name: Emmanuel Macario <macarioe@student.unimelb.edu.au>
  * Student Number: 831659
- * Last Modified: 21/05/18
+ * Filename: certcheck.h
+ * Last Modified: 23/05/18
  *
  */
-
 #ifndef CERTCHECK_H
 #define CERTCHECK_H
 
-// Program constants.
-#define MAX_LENGTH 1000
-#define BC_DEFAULT "CA:FALSE"
-#define EKU_DEFAULT "TLS Web Server Authentication"
-#define RSA_KEYSIZE_DEFAULT 2048
+// PROGRAM CONSTANTS
+
+#define MAX_LINE_LEN 1000
+#define BASIC_CONSTRAINTS_DEFAULT "CA:FALSE"
+#define EXTENDED_KEY_USAGE_DEFAULT "TLS Web Server Authentication"
+#define RSA_MINIMUM_KEYSIZE 2048
+
+
+// FUNCTION PROTOYPES
+
+// Takes as input an X.509 certificate, and returns
+// true if the respective RSA key length is greater than 
+// or equal to the default minimum length of 2048 bits.
+bool validate_rsa_key_size(X509 *cert);
+
+
+// Takes as input an X.509 certificate and
+// returns if the 'Not Before' date is valid,
+// otherwise returns false.
+bool validate_not_before(X509 *cert);
+
+
+// Takes as input an X.509 certificate and
+// returns if the 'Not After' date is valid,
+// otherwise returns false.
+bool validate_not_after(X509 *cert);
+
+
+// Takes as input an X.509 certificate and returns true
+// if both the 'Not Before' and 'Not After' dates are valid,
+// otherwise returns false.
+bool validate_dates(X509 *cert);
+
+
+// Takes as input a domain name as seen in the second column of the input
+// csv file, and validates it against another DNS name 'pattern', which may 
+// or may not include a wilcard. Returns true if there is a match, otherwise
+// returns false.
+bool matches(char *domain_name, char *pattern);
+
+
+// Performs a validation check to see if a given domain name
+// matches the certificate's 'Common Name' subject field entry. 
+// Returns true if there is a match, otherwise returns false.
+bool matches_common_name(char *domain_name, X509 *cert);
+
+
+// Performs a validation check to see if a given domain name
+// matches any of the hostnames in the certificate's 'Subject
+// Alternative Name' extension.
+bool matches_subject_alt_name(char *domain_name, X509 *cert);
+
+
+// Performs a validation check to see if the given domain name
+// matches the certificate's 'Common Name' entry, or any of the hostnames 
+// in the certificate's 'Subject Alternative Name' extension. Returns
+// true if the certificate is valid for the given domain name, otherwise
+// returns false.
+bool validate_domain_name(char *domain_name, X509 *cert);
+
+
+// Performs a validation check to see if the certificate's 'Basic Constraints'
+// extension contains the value 'CA:FALSE'. Returns true if so, otherwise 
+// returns false.
+bool validate_basic_constraints(X509 *cert);
+
+
+// Performs a validation check to see if the certificate's 'Extended Key Usage'
+// extension contains the value 'TLS Web Server Authentication'. Returns true 
+// if so, otherwise returns false.
+bool validate_ext_key_usage(X509 *cert);
+
+
+// Takes as input the name of a file containing a Base64 encoded X.509
+// certificate, and validates it with respect to associated domain name 
+// given in the second column of the input csv file. Also validates the
+// certificate's dates, RSA key size, basic constraints and extended key 
+// usage values. Returns 1 if the certificate is valid, otherwise returns 0.
+int validate_certificate(char *filename, char *domain_name);
+
 
 #endif
